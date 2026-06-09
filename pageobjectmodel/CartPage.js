a4194725ea051f8d4eb44562fc3aee5692abf792
vikenty19 <vikenty.plakhov@testpro.io>
2026-06-09 16:48:16 -0400

    CartPage method check product in the cart updated

--- pageobjectmodel/CartPage.js
+++ pageobjectmodel/CartPage.js
@@ -7,14 +7,23 @@ class CartPage{
         this.checkoutButton=page.locator("ul button[type='button'].btn-primary");
     }
     async checkProductInCart(productName){
-        await this.cartProducts.first().waitFor();
-        const cartItems=await this.cartProducts.allTextContents();
-        console.log(cartItems);
-        // const bool =await cartItems.includes(productName);
-        // await expect(bool).toBeTruthy()
-    await expect(
-        this.page.locator(`h3:has-text("${productName}")`)
-    ).toBeVisible();
+    //     await this.cartProducts.first().waitFor();//it doesn't workin CI/CD
+        
+    //     const cartItems=await this.cartProducts.allTextContents();
+    //     console.log(cartItems);
+    //     // const bool =await cartItems.includes(productName);
+    //     // await expect(bool).toBeTruthy()
+    // await expect(
+    //     this.page.locator(`h3:has-text("${productName}")`)
+    // ).toBeVisible();
+
+    //another way to check the product in cart
+        const productInCart = this.page
+            .locator(".cartSection h3")
+            .filter({ hasText: productName });
+
+        await expect(productInCart).toBeVisible();
+    
 }
  
         async goToCheckout(){  
